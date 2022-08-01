6ce50f2583e13980526c3ef7014a2e46b77581cc
Vamsi reddy <vamsireddytalla@gmail.com>
2022-08-01 16:22:40 +0530

    second commit

--- app/src/main/java/com/example/tablayoutslide/MainActivity.java
+++ app/src/main/java/com/example/tablayoutslide/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.tablayoutslide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.tablayoutslide.databinding.ActivityMainBinding;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -18,6 +20,23 @@ public class MainActivity extends AppCompatActivity
         for (int i = 0; i < 10; i++) {
             binding.tabRoot.addTab(binding.tabRoot.newTab().setText("Tab "+i));
         }
+
+        binding.tabRoot.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Toast.makeText(MainActivity.this, tab.getText(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
     }
 }
